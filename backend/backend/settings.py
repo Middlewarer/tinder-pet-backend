c9add2f77bb8f493adc807ff6eca5ec42805cac2
Middlewarer | Grabchuk Alex <mranarchy@mail.ru>
2026-04-19 02:18:19 +0300

    prdeploy 2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -86,8 +86,8 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
+    
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
